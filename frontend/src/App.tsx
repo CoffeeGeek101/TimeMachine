481553254cc9a79239839b1355b58c1ffd5ae65c
CoffeeGeek101 <workwithshoumya@gmail.com>
2025-12-23 20:13:00 +0530

    add : configs

---
+++ frontend/src/App.tsx
@@ -0,0 +1,14 @@
+import './App.css'
+import Timemachine from './pages/TimeMachine.tsx/Timemachine';
+
+function App() {
+
+  return (
+  // <div>
+  //   <Pipeline initialData={[3,4,2,3]} initialGlobalData={0} pointer={2} />
+  // </div>
+  <Timemachine/>
+  )
+}
+
+export default App
